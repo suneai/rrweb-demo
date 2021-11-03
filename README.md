cefd6cb9be0f1c0e8b0272038a55824daad885a7
sun.xiaoye <sun.xiaoye@mwee.cn>
2021-11-03 17:07:16 +0800

    说明修改

--- README.md
+++ README.md
@@ -1,6 +1,48 @@
+
+# rrweb 打开web页面录制与回放的黑盒
+#### `web` 录制回放基础库`rrweb`，它是将页面中的DOM以及用户操作保存为可序列化的数据，用以实现远程回放。用于前端的异常监控以及对应数据的收集。
+
+[rrweb](https://github.com/rrweb-io/rrweb/blob/master/guide.zh_CN.md)
+#### 基于`LogRocket`一个像素级的录制与回放工具，做的二次封装。
+
+[LogRocket](https://logrocket.com/)
+
+### 应用场景
+1. 记录用户使用产品的方式并加以分析，进一步优化产品
+2. 采集用户遇到的bug操作路径，予以复现
+3. 记录CI环境中的e2e测试的执行情况。
+4. 录制体积更小、清晰无损的产品演示。
+
+### 回放的基础：DOM快照
+页面中的视图状态可以通过 DOM 树的形式描述，所以当我们尝试录制⼀个⻚⾯时，我们实际上是在记录 DOM 树在各个时间点上的状态，在 rrweb 中我们称⼀次这样的状态记录为⼀个快照。
+
+### 序列化
+`非标准的序列化`
+1. 去脚本化，被录制⻚⾯中的所有 JavaScript 都不应该被执⾏。
+
+2. 记录没有反映在 HTML 中的视图状态。例如 <input type="text" /> 输⼊后的值不会反映在其 HTML中，我们需要读取其 value 值并加以记录。
+
+3. 相对路径转换为绝对路径。回放时⻚⾯ URL为重放⻚⾯的地址，如果被录制⻚⾯中有⼀些相对路径就会产⽣错误。
+
+4. 尽量记录 CSS 样式表的内容。如果被录制⻚⾯加载了⼀些同源的样式表，我们则可以获取到解析好的 CSS rules，录制时将能获取到的样式都 inline 化，这样可以让⼀些内⽹环境（如 localhost）的录制也有⽐较好的回放效果。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # rrweb 使用demo
-[API指南](https://github.com/rrweb-io/rrweb/blob/master/guide.zh_CN.md)
 [参考文档](https://zhuanlan.zhihu.com/p/60639266)
+[参考文档](https://juejin.cn/post/6844903925213036552)
 
 [react的路由配置](https://blog.csdn.net/hbiao68/article/details/98736501)
 
--- src/pages/rrweb/index.js
+++ src/pages/rrweb/index.js
@@ -3,16 +3,18 @@ import { Space, Card, Button, Input } from 'antd';
 import { RedditOutlined, UserDeleteOutlined } from '@ant-design/icons';
 import { record , Replayer } from 'rrweb'
 import rrwebPlayer from 'rrweb-player'
+import luImg from '../../images/lu.webp'
+import './index.css'
 
 const { Search } = Input;
 
 let stopFun = null
+let eventsList = []
 class RRWebPage extends Component {
     constructor(props) {
         super(props)
         this.state = {
             listData: [],
-            eventsList: []
         }
         this.playRef = null
     }
@@ -21,12 +23,12 @@ class RRWebPage extends Component {
     // 录制
     handleRecord = () => {
         let tempEvents = []
-        const self = this
         stopFun = record({
             emit(event) {
+                console.info(event,'记录 DOM 树在各个时间点上的状态')
                 // 保存获取到的 event 数据
                 tempEvents.push(event)
-                self.setState({ eventsList: tempEvents })
+                eventsList = tempEvents
             }
         })
     }
@@ -36,7 +38,6 @@ class RRWebPage extends Component {
         // 停止录制
         stopFun && stopFun()
         const self = this
-        const { eventsList } = this.state
         console.log(eventsList)
         new rrwebPlayer({
             target: self.playRef,
@@ -45,14 +46,11 @@ class RRWebPage extends Component {
                 showController: false,
             },
           })
-        // const events = eventsList
-        // const replayer = new Replayer(events);
-        // replayer.play();
-
     }
 
     // 添加行
     handleAdd = (e) => {
+        // e.stopPropagation()
         const { listData } = this.state
         let tempList = listData
         if (e) {
@@ -75,7 +73,7 @@ class RRWebPage extends Component {
         return listData.map((item, index) => (
             <Space key={index} style={{ margin: '10px 0', textAlign: 'center', border: '1px dotted #AAA' }}>
                 <RedditOutlined style={{ margin: '0 10px' }} />
-                <span style={{ display: 'inline-block', width: '35px', overflow: 'hidden' }}>{item}</span>
+                <span style={{ display: 'inline-block', width: '25px', overflow: 'hidden' }}>{item}</span>
                 <UserDeleteOutlined onClick={() => this.deleteItem(index)} style={{ marginLeft: 160, paddingRight: 10 }} />
             </Space>
         ))
@@ -89,7 +87,7 @@ class RRWebPage extends Component {
                     <Button onClick={this.handleRecord} type="primary">录制</Button>
                     <Button onClick={this.handleReplay}>回放</Button>
                 </Space>
-                <Space size={8} direction="vertical">
+                <Space size={8}>
                     <Card title="edit" style={{ width: 300 }}>
                         <Search
                             placeholder="input search text"
@@ -104,8 +102,13 @@ class RRWebPage extends Component {
                                 <p style={{ margin: '10px 0', padding: '30px 0', textAlign: 'center', border: '1px dotted #AAA' }}>暂无数据</p>
                         }
                     </Card>
-                    <Card title="replay" style={{ width: 300 }}>
-                        <p ref={ nodeRef => this.playRef = nodeRef }></p>
+                    <Card title="replay" style={{ Width: 300 }}>
+                        <img src={luImg} style={{ Width: 300 }} alt='lu'/>
+                    </Card>
+                </Space>
+                <Space size={8}>
+                    <Card title="replay" style={{ minWidth: 300 }}>
+                        <div ref={ nodeRef => this.playRef = nodeRef }></div>
                     </Card>
                 </Space>
             </Space>
