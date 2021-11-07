aff1faddbc8730c31abf853d459b8d796877458c
孙晓叶 <sunxy@gantsoftware.com>
2021-11-07 15:37:40 +0800

    样式调整

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,28 +1,13 @@
-import React , { useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';import { Layout, Menu, Breadcrumb } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import React , { } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Layout } from 'antd';
 import { ComponentsRouter } from '../../config/routerConfig';
-import { headerNavList , subMenuList } from './schame'
 import './index.css';
 
-const { SubMenu } = Menu;
-const { Header, Content, Footer, Sider } = Layout;
+const { Content } = Layout;
 
 
 const IndexPage = () => {
-
-    const [currentNav,setCurrentNav] = useState(['sune'])
-    const [currentSubMenuItem,setCurrentSubMenuItem] = useState(['rrweb'])
-    const navView = () => {
-        return headerNavList.map((item) => (
-            <Menu.Item key={item.name} onClick={() => setCurrentNav([item.name])}>{item.name}</Menu.Item>
-        ))
-    }
-    const navsubMenuView = () => {
-        return subMenuList.map((item) => (
-            <Menu.Item key={item.name} onClick={() => setCurrentSubMenuItem([item.name])}>{item.name}</Menu.Item>
-        ))
-    }
 
     const routerView = () => {
         return ComponentsRouter.map((item, index) => (
@@ -32,34 +17,12 @@ const IndexPage = () => {
 
     return (
         <Layout>
-            <Header className="header">
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={currentNav}>
-                    {navView()}
-                </Menu>
-            </Header>
-            <Content style={{ padding: '0 30px' }}>
-                <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
-                    <Sider className="site-layout-background" width={200}>
-                        <Menu
-                            mode="inline"
-                            defaultSelectedKeys={['rrweb']}
-                            defaultOpenKeys={currentSubMenuItem}
-                            style={{ height: '100%' }}
-                        >
-                            <SubMenu key="sub1" icon={<UserOutlined />} title="关于rrweb的demo">
-                                {navsubMenuView()}
-                            </SubMenu>
-                        </Menu>
-                    </Sider>
-                    <Content style={{ padding: '0 24px', minHeight: 280 }}>
-                        <Switch>
-                            {routerView()}
-                            <Redirect to='/' />
-                        </Switch>
-                    </Content>
-                </Layout>
+            <Content style={{ padding: '24px', minHeight: 280 }}>
+                <Switch>
+                    {routerView()}
+                    <Redirect to='/' />
+                </Switch>
             </Content>
-            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
         </Layout>
     )
 
--- src/pages/rrweb/index.js
+++ src/pages/rrweb/index.js
@@ -1,119 +1,184 @@
-import React, { Component } from 'react'
-import { Space, Card, Button, Input } from 'antd';
-import { RedditOutlined, UserDeleteOutlined } from '@ant-design/icons';
-import { record , Replayer } from 'rrweb'
-import rrwebPlayer from 'rrweb-player'
-import luImg from '../../images/lu.webp'
-import './index.css'
-
+import React, { Component } from "react";
+import { Layout, Space, Card, Button, Input } from "antd";
+import { RedditOutlined, UserDeleteOutlined } from "@ant-design/icons";
+import { record, Replayer } from "rrweb";
+import rrwebPlayer from "rrweb-player";
+import luImg from "../../images/lu.webp";
+import "./index.css";
+const { Sider, Content } = Layout;
 const { Search } = Input;
 
-let stopFun = null
-let eventsList = []
+let stopFun = null;
+let eventsList = [];
 class RRWebPage extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            listData: [],
-        }
-        this.playRef = null
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      listData: [],
+    };
+    this.playRef = null;
+  }
 
-    // [录制与回放](https://github.com/rrweb-io/rrweb/blob/master/docs/recipes/record-and-replay.zh_CN.md)
-    // 录制
-    handleRecord = () => {
-        let tempEvents = []
-        stopFun = record({
-            emit(event) {
-                console.info(event,'记录 DOM 树在各个时间点上的状态')
-                // 保存获取到的 event 数据
-                tempEvents.push(event)
-                eventsList = tempEvents
-            }
-        })
-    }
+  // [录制与回放](https://github.com/rrweb-io/rrweb/blob/master/docs/recipes/record-and-replay.zh_CN.md)
+  // 录制
+  handleRecord = () => {
+    let tempEvents = [];
+    stopFun = record({
+      emit(event) {
+        console.info(event, "记录 DOM 树在各个时间点上的状态");
+        // 保存获取到的 event 数据
+        tempEvents.push(event);
+        eventsList = tempEvents;
+      },
+    });
+  };
 
-    // 回放
-    handleReplay = () => {
-        // 停止录制
-        stopFun && stopFun()
-        const self = this
-        console.log(eventsList)
-        new rrwebPlayer({
-            target: self.playRef,
-            props: {
-                events: eventsList,
-                showController: false,
-            },
-          })
-    }
+  // 回放
+  handleReplay = () => {
+    // 停止录制
+    stopFun && stopFun();
+    const self = this;
+    console.log(eventsList);
+    new rrwebPlayer({
+      target: self.playRef,
+      props: {
+        events: eventsList,
+        showController: false,
+      },
+    });
+  };
 
-    // 添加行
-    handleAdd = (e) => {
-        // e.stopPropagation()
-        const { listData } = this.state
-        let tempList = listData
-        if (e) {
-            tempList.push(e)
-            this.setState({ listData: tempList })
-        }
+  // 添加行
+  handleAdd = (e) => {
+    // e.stopPropagation()
+    const { listData } = this.state;
+    let tempList = listData;
+    if (e) {
+      tempList.push(e);
+      this.setState({ listData: tempList });
     }
+  };
 
-    // 删除行
-    deleteItem = (index) => {
-        const { listData } = this.state
-        let tempList = listData
-        tempList.splice(index, 1)
-        this.setState({ listData: tempList })
-    }
+  // 删除行
+  deleteItem = (index) => {
+    const { listData } = this.state;
+    let tempList = listData;
+    tempList.splice(index, 1);
+    this.setState({ listData: tempList });
+  };
 
-    // 行view
-    listView = () => {
-        const { listData } = this.state
-        return listData.map((item, index) => (
-            <Space key={index} style={{ margin: '10px 0', textAlign: 'center', border: '1px dotted #AAA' }}>
-                <RedditOutlined style={{ margin: '0 10px' }} />
-                <span style={{ display: 'inline-block', width: '25px', overflow: 'hidden' }}>{item}</span>
-                <UserDeleteOutlined onClick={() => this.deleteItem(index)} style={{ marginLeft: 160, paddingRight: 10 }} />
-            </Space>
-        ))
-    }
+  // 行view
+  listView = () => {
+    const { listData } = this.state;
+    return listData.map((item, index) => (
+      <Space
+        key={index}
+        style={{
+          margin: "10px 0",
+          textAlign: "center",
+          border: "1px dotted #AAA",
+        }}
+      >
+        <RedditOutlined style={{ margin: "0 10px" }} />
+        <span
+          style={{
+            display: "inline-block",
+            width: "45px",
+            height: "26px",
+            lineHeight: "26px",
+            fontSize: "12px",
+            overflow: "hidden",
+          }}
+        >
+          {item}
+        </span>
+        <UserDeleteOutlined
+          onClick={() => this.deleteItem(index)}
+          style={{ marginLeft: 10, paddingRight: 10 }}
+        />
+      </Space>
+    ));
+  };
 
-    render() {
-        const { listData } = this.state
-        return (
-            <Space direction="vertical" className="site-layout-background" style={{ padding: 24, width:'100%', minHeight: '82vh', background: '#Fff' }}>
-                <Space size={10}>
-                    <Button onClick={this.handleRecord} type="primary">录制</Button>
-                    <Button onClick={this.handleReplay}>回放</Button>
-                </Space>
-                <Space size={8}>
-                    <Card title="edit" style={{ width: 300 }}>
-                        <Search
-                            placeholder="input search text"
-                            allowClear
-                            enterButton="添加"
-                            size="samll"
-                            onSearch={(e) => this.handleAdd(e)}
-                        />
-                        {
-                            (listData.length > 0) ?
-                                this.listView() :
-                                <p style={{ margin: '10px 0', padding: '30px 0', textAlign: 'center', border: '1px dotted #AAA' }}>暂无数据</p>
-                        }
-                    </Card>
-                    <Card title="replay" style={{ Width: 300 }}>
-                        <img src={luImg} style={{ Width: 300 }} alt='lu'/>
-                    </Card>
-                </Space>
-                <Space size={8}>
-                    <Card title="replay" style={{ minWidth: 300 }}>
-                        <div ref={ nodeRef => this.playRef = nodeRef }></div>
-                    </Card>
-                </Space>
-            </Space>
-        )
-    }
+  render() {
+    const { listData } = this.state;
+    return (
+      <Layout
+        direction="vertical"
+        className="site-layout-background"
+        style={{
+          padding: 24,
+          width: "100%",
+          minHeight: "94vh",
+          background: "#Fff",
+        }}
+      >
+        <Sider
+          width={300}
+          style={{ background: "#fff" }}
+          className="site-layout-background"
+        >
+          <Space size={10} style={{ margin: "5px auto" }}>
+            <Button onClick={this.handleRecord} type="primary">
+              录制
+            </Button>
+            <Button onClick={this.handleReplay}>回放</Button>
+          </Space>
+          <Space size={8} direction="vertical" style={{ margin: "5px auto" }}>
+            <Card
+              title="edit"
+              style={{
+                width: 240,
+                height: 200,
+                overflowX: "hidden",
+                overflowY: "scroll",
+              }}
+            >
+              <Search
+                placeholder="input search text"
+                allowClear
+                enterButton="添加"
+                size="samll"
+                onSearch={(e) => this.handleAdd(e)}
+              />
+              {listData.length > 0 ? (
+                this.listView()
+              ) : (
+                <p
+                  style={{
+                    margin: "10px 0",
+                    padding: "30px 0",
+                    textAlign: "center",
+                    border: "1px dotted #AAA",
+                  }}
+                >
+                  暂无数据
+                </p>
+              )}
+            </Card>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                />
+              }
+            >
+              <Card.Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+          </Space>
+        </Sider>
+        <Content>
+          <div ref={(nodeRef) => (this.playRef = nodeRef)}></div>
+        </Content>
+      </Layout>
+    );
+  }
 }
 
-export default RRWebPage
+export default RRWebPage;
