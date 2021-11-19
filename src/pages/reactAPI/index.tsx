5aa09d099ce83595f13f33571bcc48cb06c3edb3
sun.xiaoye <sun.xiaoye@mwee.cn>
2021-11-19 15:52:46 +0800

    关于react的ref

--- src/pages/reactAPI/index.tsx
+++ src/pages/reactAPI/index.tsx
@@ -11,19 +11,23 @@ const ReactDemo = (props: { [x: string]: any; }) => {
 
     return (
         <Space direction="vertical">
-            <Card size="small" title="ref 回调函数" style={{ width: 300 }}>
-                <div>ref回调函数是为类设置一个属性来访问DOM元素的常见模式,接收一个基础的DOM元素作为其参数,来存储对DOM节点的引用</div>
-                <RefItem />
-            </Card>
-            <Card size="small" title="ref与class组件" style={{ width: 300 }}>
-                <div>当 ref 被传递给 render 中的元素时，对该节点的引用可以在 ref 的 current 属性中被访问</div>
-                <ClassRefItem />
-            </Card>
-            <Card size="small" title="ref与函数组件" style={{ width: 300 }}>
-                <div>Refs 是使用 React.createRef() 创建的，并通过 ref 属性附加到 React 元素</div>
-                <div>当 ref 被传递给 render 中的元素时，对该节点的引用可以在 ref 的 current 属性中被访问</div>
-                <ForwardRefItem ref={fRef} />
-            </Card>
+            <div>关于ref</div>
+            <Space size={8}>
+                <Card size="small" title="ref 回调函数" style={{ width: 300 }}>
+                    <div>ref回调函数是为类设置一个属性来访问DOM元素的常见模式,这个函数中接受 React 组件实例或 HTML DOM 元素作为参数，以使它们能在其他地方被存储和访问.</div>
+                    <div>如果是组件作为参数，可以直接调用组件上的方法，或取其参数，如果是在函数组件里面，可以在current上访问方法或属性</div>
+                    <RefItem />
+                </Card>
+                <Card size="small" title="ref与class组件" style={{ width: 300 }}>
+                    <div>当 ref 被传递给 render 中的元素时，对该节点的引用可以在 ref 的 current 属性中被访问</div>
+                    <ClassRefItem />
+                </Card>
+                <Card size="small" title="ref与函数组件" style={{ width: 300 }}>
+                    <div>Refs 是使用 React.createRef() 创建的，并通过 ref 属性附加到 React 元素</div>
+                    <div>当 ref 被传递给 render 中的元素时，对该节点的引用可以在 ref 的 current 属性中被访问</div>
+                    <ForwardRefItem ref={fRef} />
+                </Card>
+            </Space>
         </Space>
     )
 }
